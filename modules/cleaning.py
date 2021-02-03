c84e20fddd3c1c2981f56df1dd4485a49512775f
Sara Ghasemi <ghasemi.sarah@gmail.com>
2021-02-03 16:14:32 +0100

    adding new functions to cleaning module and updating my notebook

--- modules/cleaning.py
+++ modules/cleaning.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 def merge(df_train, df_store):
     '''
-    Merge train data with store data based on
-    values in Stores column
+    Merge train data with store data based on values in Stores column.
     '''
     full_df = df_train.merge(df_store, how='left', on='Store')
     return full_df
@@ -11,14 +10,26 @@ def merge(df_train, df_store):
 def drop_column(df, column='Customers'):
     '''
     Remove the Customers column as instructed by competition instructors.
+    Returns a new dataframe.
     '''
     df_new = df.drop(str(column), axis=1)
     return df_new
 
+def clean_column_values(df, column='StateHoliday'):
+    '''
+    In the StateHoliday column, transform 0.0 to 0 and all column to string.
+    Returns a new dataframe.
+    '''
+    df_new = df.copy()
+    #Remove the values where StateHoliday is NaN?
+    df_new = df_new.dropna(subset=[str(column)])
+    df_new[str(column)] = df_new[str(column)].apply(lambda x: str(int(x)) if x == 0.0 or x == 0 else x)
+    return df_new
 
 def drop_null_targets(df, target='Sales'):
     '''
-    Remove the rows where value for Sales is null
+    Remove the rows where value for Sales is null.
+    Returns a new dataframe.
     '''
     null_target = df[df[str(target)].isnull()]
     df_new = df[~df.index.isin(null_target.index)]
@@ -27,6 +38,7 @@ def drop_null_targets(df, target='Sales'):
 def drop_zero_targets(df, target='Sales'):
     '''
     Remove the rows with zero Sales.
+    Returns a new dataframe.
     '''
     zero_target = df[df[str(target)] == 0]
     df_new = df[~df.index.isin(zero_target.index)]
@@ -35,6 +47,7 @@ def drop_zero_targets(df, target='Sales'):
 def clean_targets(df, target='Sales'):
     '''
     Drop all the rows where Sales does is null or zero.
+    Returns a new dataframe.
     '''
     df_new = drop_null_targets(df, target=str(target))
     df_new = drop_zero_targets(df, target=str(target))
@@ -42,7 +55,7 @@ def clean_targets(df, target='Sales'):
 
 def count_null_features(df):
     '''
-    Count how many null values are in each column
+    Count how many null values are in each column.
     '''
     tot_rows = df.shape[0]
     for column in df.columns:
@@ -50,54 +63,35 @@ def count_null_features(df):
         frac = null_rows / tot_rows
         print(f"Column {str(column)} has {frac * 100 : .0f}% null values.")
 
-
-def drop_null_features(df, threshold=0.03, verbose=False):
+def rough_features_cleaning(df, threshold=0.10, drop_columns=True, verbose=False):
     '''
-    Remove the null features from a column if number of rows with null
-    values are very small, i.e. less than a certain percentage of the
-    full data rows.
-    The default threshold is 3%.
-    '''
-    tot_rows = df.shape[0]
-    if verbose:
-        print("Total number of rows in full data set: ", tot_rows)
-    for column in df.columns:
-        null_rows = df.loc[df.loc[:, str(column)].isnull()].shape[0]
-        frac = null_rows / tot_rows
-        if verbose:
-            print(f"Column {str(column)} has {frac * 100 : .0f}% null values.")
-        if frac > 0 and frac <= threshold:
-            df = df.loc[~df.loc[:, str(column)].isnull()]
-            if verbose:
-                print(f"Removed rows with null value from {str(column)}.")
-    if verbose:
-        print("Total number of rows in clean data set: ", df.shape[0])
-    return df
-
-def rough_cleaning(df, threshold=0.1, verbose=False):
-    '''
-    Remove the null features from a column if number of rows with null
-    feature is less than a certain percentage of the full data rows
-    (i.e. the threshold), drop the whole column if number of rows with
-    null feature is larger than the threshold.
+    Remove the rows with null feature values if number of null values
+    are very small.
+    When the flag is true, drop the whole feature column if number of
+    null values are not small.
     The default threshold is 10%.
+    Transform the values in StateHoliday column.
+    Returns a new dataframe.
     '''
-    tot_rows = df.shape[0]
-    if verbose:
-        print("Total number of rows in full data set: ", tot_rows)
-    for column in df.columns:
-        null_rows = df.loc[df.loc[:, str(column)].isnull()].shape[0]
+    df_new = df.copy()
+    tot_rows = df_new.shape[0]
+    print("Total number of rows before cleaning: ", tot_rows)
+    for column in df_new.columns:
+        null_rows = df_new.loc[df_new.loc[:, str(column)].isnull()].shape[0]
         frac = null_rows / tot_rows
         if verbose:
             print(f"Column {str(column)} has {frac * 100 : .0f}% null values.")
         if frac > 0 and frac <= threshold:
-            df = df.loc[~df.loc[:, str(column)].isnull()]
+            df_new = df_new.loc[~df_new.loc[:, str(column)].isnull()]
             if verbose:
                 print(f"Removed rows with null value from {str(column)}.")
-        elif frac > threshold:
-            df = df.drop(str(column), axis=1)
+        if drop_columns and frac > threshold:
+            df_new = df_new.drop(str(column), axis=1)
             if verbose:
                 print(f"Dropped the column {str(column)}.")
-    if verbose:
-        print("Total number of rows in clean data set: ", df.shape[0])
-    return df
+    print("Total number of rows after cleaning: ", df_new.shape[0])
+
+    df_new = clean_column_values(df_new, column='StateHoliday')
+
+    return df_new
+
